60767a55ca922f09ed8a99da0fedd97d0c1d9f46
amaganadonald <donald9do@gmail.com>
2024-09-30 09:03:27 +0200

    Commit base application

---
+++ src/main/java/com/amagana/librairie_modulith/LibrairieModulithApplication.java
@@ -0,0 +1,13 @@
+package com.amagana.librairie_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrairieModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrairieModulithApplication.class, args);
+	}
+
+}
